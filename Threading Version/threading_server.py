008306cb1cb66b0e68e8a4613f5e34a924b6dd68
jupiterbjy <jupiterbjy@gmail.com>
2020-07-05 23:50:45 +0900

    Refactoring Server code to use separated read-write queue for main server. Added keyboardInterrupt to server.

--- Threading Version/threading_server.py
+++ Threading Version/threading_server.py
@@ -1,6 +1,7 @@
 import threading
 import socket
 import pickle
+from itertools import chain
 from queue import Queue
 try:
     from SharedData import SharedModules
@@ -8,6 +9,8 @@ except ImportError:
     from sys import path
     path.insert(1, '..')
     from SharedData import SharedModules
+
+# TODO: move global variables to locals.
 
 
 # setup
@@ -40,19 +43,36 @@ def generate_queue():
     return q
 
 
-def worker(id_, q: Queue):
-    while not q.empty():
-        p: int = q.get()
+def worker(id_, q, send, recv, event: threading.Event):
+    q: Queue
+    send: Queue
+    recv: Queue
 
+    while not q.empty():
+
+        # check eject event.
+        if event.is_set():
+            print(f"[SS{id_:2}][Warn] Worker {id_} stopping.")
+            return
+
+        # get next work.
+        p: int = q.get()
+        q.task_done()
+
+        # check if port is in blacklist.
         if p in config.EXCLUDE:
             print(f"[SS{id_:2}][Info] Skipping Port {p}.")
             continue
 
         # receive worker announcement.
-        worker_id = read_b(conn.recv(1024))
+        worker_id = recv.get()
+        recv.task_done()
+
         print(f"[SS{id_:2}][Info] Worker {worker_id} announce READY.")
         print(f"[SS{id_:2}][Info] Sending port {p} to Client.")
-        conn.send(write_b(p))
+
+        send.put(p)
+        send.task_done()
 
         print(f"[SS{id_:2}][Info] Opening Port {p}.")
         child_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -75,7 +95,6 @@ def worker(id_, q: Queue):
             print(f"[SS{id_:2}][Info] Port {p} is open.")
 
         child_sock.close()
-        q.task_done()
 
     # Send end signal to client.
     # first worker catching this signal will go offline.
@@ -83,29 +102,61 @@ def worker(id_, q: Queue):
     conn.send(end.encode(config.ENCODING))
 
 
+def send_thread(q: Queue, e: threading.Event):
+    while not e.is_set():
+        if q.empty():
+            continue
+
+        n = q.get()
+        conn.send(write_b(n))
+
+
+def recv_thread(q: Queue, e: threading.Event):
+    while not e.is_set():
+        q.put(read_b(conn.recv(1024)))
+
+
 def main():
     # just wrapping in function makes global to local variable, runs faster.
 
+    event = threading.Event()
     work = generate_queue()
+
+    send_q = Queue()
+    recv_q = Queue()
+
+    server_thread = [
+        threading.Thread(target=send_thread, args=[send_q, event]),
+        threading.Thread(target=recv_thread, args=[recv_q, event])
+    ]
+
     workers = [
         threading.Thread(target=worker, args=[i, work]) for i in range(config.WORKERS)
     ]
 
-    for w in workers:
+    for w in chain(server_thread, workers):
         w.start()
 
-    for w in workers:
-        w.join()
+    try:
+        for w in workers:
+            w.join()
 
-    used_data = pickle.dumps([USED_PORTS, SHUT_PORTS])
-    conn.send(used_data)
-    conn.close()
+    except KeyboardInterrupt:
+        event.set()
+        for w in chain(workers, server_thread):
+            w.join()
+        print("[S][Warn] All workers stopped.")
 
-    print("\n[Results]")
-    print(f"Used Ports  : {USED_PORTS}")
-    print(f"Closed Ports: {SHUT_PORTS}")
-    print(f"Excluded    : {config.EXCLUDE}")
-    print(f"\nAll other ports from 1~{config.PORT_MAX} is open.")
+    else:
+        used_data = pickle.dumps([USED_PORTS, SHUT_PORTS])
+        conn.send(used_data)
+        conn.close()
+
+        print("\n[Results]")
+        print(f"Used Ports  : {USED_PORTS}")
+        print(f"Closed Ports: {SHUT_PORTS}")
+        print(f"Excluded    : {config.EXCLUDE}")
+        print(f"\nAll other ports from 1~{config.PORT_MAX} is open.")
 
 
 if __name__ == "__main__":
