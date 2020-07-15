cfe2b30f883b9b23ab64a248095313fc08e30b6c
jupiterbjy <jupiterbjy@gmail.com>
2020-07-15 11:46:30 +0900

    Prepare for colorizing. Will colorize output when colorama is loaded.

--- SharedData/SharedModules.py
+++ SharedData/SharedModules.py
@@ -4,6 +4,14 @@ from urllib import request
 from types import SimpleNamespace
 import json
 
+try:
+    import colorama
+except ModuleNotFoundError:
+    COLOR = False
+else:
+    colorama.init()
+    COLOR = True
+
 
 loc = path.dirname(path.abspath(__file__))
 
@@ -19,14 +27,14 @@ def set_working_dir(file=__file__):
 
 
 def get_external_ip():
-    req = request.urlopen('https://api.ipify.org')
+    req = request.urlopen("https://api.ipify.org")
     data = req.read()
-    return data.decode('utf-8')
+    return data.decode("utf-8")
 
 
 # assuming file is placed in same structure.
 def load_config():
-    with open(loc + '/config.json') as f:
+    with open(loc + "/config.json") as f:
         data = json.load(f)
         return SimpleNamespace(**data)
 
@@ -63,3 +71,23 @@ def any_thread_alive(lst: list):  # list containing threads.
         return True
 
     return False
+
+
+def Colorize(txt, color):
+    if not COLOR:
+        return txt
+
+    ansi = {
+        "RED": "\033[91m",
+        "GREEN": "\033[92m",
+        "BLUE": "\033[94m",
+        "YELLOW": "\033[93m",
+        "PURPLE": "\033[94m",
+        "CYAN": "\033[96m",
+        "END": "\033[0m",
+        "BOLD": "\033[1m",
+        "HEADER": "\033[95m",
+        "UNDERLINE": "\033[4m",
+    }
+    s = str(txt)
+    return ansi[color] + s + ansi["END"]
