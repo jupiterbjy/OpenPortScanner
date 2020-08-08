278fc369becb6d30bf7928e203b7bb56b1a39b34
jupiterbjy <jupiterbjy@gmail.com>
2020-08-08 15:33:00 +0900

    Refactoring module structures.

---
+++ SharedData/ColorSupport.py
@@ -0,0 +1,69 @@
+
+
+def colorize_closure():
+    try:
+        import colorama
+
+    except ModuleNotFoundError:
+        print("colorama not installed, disabling colored text.")
+        return lambda txt, color: str(txt)
+
+    else:
+        colorama.init()
+
+        ansi_table = {
+            "RED": "\033[91m",
+            "GREEN": "\033[92m",
+            "BLUE": "\033[94m",
+            "YELLOW": "\033[93m",
+            "PURPLE": "\033[94m",
+            "CYAN": "\033[96m",
+            "END": "\033[0m",
+            "BOLD": "\033[1m",
+            "HEADER": "\033[95m",
+            "UNDERLINE": "\033[4m",
+        }
+
+        def ansi_wrapper(txt, color):
+            return ansi_table[color] + str(txt) + ansi_table["END"]
+
+        return ansi_wrapper
+
+
+colorize = colorize_closure()
+
+
+# anti lambda?
+def red(txt: str):
+    return colorize(txt, 'RED')
+
+
+def green(txt: str):
+    return colorize(txt, 'GREEN')
+
+
+def blue(txt: str):
+    return colorize(txt, 'BLUE')
+
+
+def cyan(txt: str):
+    return colorize(txt, 'CYAN')
+
+
+def purple(txt: str):
+    return colorize(txt, 'PURPLE')
+
+
+def bold(txt: str):
+    return colorize(txt, 'BOLD')
+
+
+def header(txt: str):
+    return colorize(txt, 'HEADER')
+
+
+def underline(txt: str):
+    return colorize(txt, 'UNDERLINE')
+
+
+__all__ = ['red', 'green', 'cyan', 'purple', 'bold', 'header', 'underline']
--- SharedData/__init__.py
+++ SharedData/__init__.py
@@ -2,17 +2,9 @@ from os import chdir, path
 from sys import argv
 from urllib import request
 from types import SimpleNamespace
+from .ColorSupport import *
 import json
 import pkgutil
-
-try:
-    import colorama
-except ModuleNotFoundError:
-    print("colorama not installed, disabling colored text.")
-    COLOR = False
-else:
-    colorama.init()
-    COLOR = True
 
 
 loc = path.dirname(path.abspath(__file__))
@@ -70,44 +62,3 @@ def alive_thread_gen(lst: list):
 
 def any_thread_alive(lst: list):  # list containing threads.
     return any(alive_thread_gen(lst))
-
-
-def colorize(txt, color):
-    if not COLOR:
-        return txt
-
-    ansi = {
-        "RED": "\033[91m",
-        "GREEN": "\033[92m",
-        "BLUE": "\033[94m",
-        "YELLOW": "\033[93m",
-        "PURPLE": "\033[94m",
-        "CYAN": "\033[96m",
-        "END": "\033[0m",
-        "BOLD": "\033[1m",
-        "HEADER": "\033[95m",
-        "UNDERLINE": "\033[4m",
-    }
-    s = str(txt)
-    return ansi[color] + s + ansi["END"]
-
-
-# anti lambda?
-def red(txt: str):
-    return colorize(txt, 'RED')
-
-
-def green(txt: str):
-    return colorize(txt, 'GREEN')
-
-
-def blue(txt: str):
-    return colorize(txt, 'BLUE')
-
-
-def cyan(txt: str):
-    return colorize(txt, 'CYAN')
-
-
-def purple(txt: str):
-    return colorize(txt, 'PURPLE')
